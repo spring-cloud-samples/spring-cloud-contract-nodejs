fcc1913e0d36cedab0bdf23456f11a0aa7b00354
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-01-24 01:02:29 +0100

    Updated the solution to just call npm test

--- json/1_request.sh
+++ json/1_request.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-curl -H "Content-Type:application/json;charset=utf-8" -X POST --data @1_request.json http://localhost:9876/api/books
+curl -H "Content-Type:application/json" -X POST --data @1_request.json http://localhost:9876/api/books
