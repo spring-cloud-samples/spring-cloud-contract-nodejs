e491c0b06bad5af72021b9926247fccb2dcd3840
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2021-02-05 14:49:23 +0100

    Updated image to 3.0.2

--- run_stub_runner_boot.sh
+++ run_stub_runner_boot.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 
-SC_CONTRACT_DOCKER_VERSION="${SC_CONTRACT_DOCKER_VERSION:-3.0.0-SNAPSHOT}"
+SC_CONTRACT_DOCKER_VERSION="${SC_CONTRACT_DOCKER_VERSION:-3.0.2-SNAPSHOT}"
 
 # Setup
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=springcloud/spring-cloud-contract-stub-runner:${SC_CONTRACT_DOCKER_VERSION} --format="{{.ID}}")) || echo "Nothing to cleanup"
